6f5c66591174057b75e2aa406aed6a6584067121
kibet <nehmz254@gmail.com>
2023-07-27 20:29:05 +0300

    What a day

--- example.c
+++ example.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-/*
- * main - Entry point.
+/**
+ * main - Entry point of the program.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	printf("\"I love pizza\" Abraham said\n");
